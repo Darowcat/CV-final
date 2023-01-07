e71b240d50aff8fbd07f422992fd43bae3e2122e
DaRowCat <jimmy890718@gmail.com>
2023-01-07 20:44:07 +0800

    modify classnum

--- model_insightface.py
+++ model_insightface.py
@@ -240,7 +240,7 @@ class MobileFaceNet(Module):
 
 class Arcface(Module):
     # implementation of additive margin softmax loss in https://arxiv.org/abs/1801.05599    
-    def __init__(self, embedding_size=512, classnum=51332,  s=64., m=0.5):
+    def __init__(self, embedding_size=512, classnum=3219,  s=64., m=0.5):
         super(Arcface, self).__init__()
         self.classnum = classnum
         self.kernel = Parameter(torch.Tensor(embedding_size,classnum))
@@ -281,7 +281,7 @@ class Arcface(Module):
     
 class Am_softmax(Module):
     # implementation of additive margin softmax loss in https://arxiv.org/abs/1801.05599    
-    def __init__(self,embedding_size=512,classnum=51332):
+    def __init__(self,embedding_size=512,classnum=3219):
         super(Am_softmax, self).__init__()
         self.classnum = classnum
         self.kernel = Parameter(torch.Tensor(embedding_size,classnum))
